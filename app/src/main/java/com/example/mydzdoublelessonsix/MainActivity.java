e80399f94dd7eb8b8ebc75ca97f118effaf787c7
iliy <gromov218974@mail.ru>
2021-09-14 20:05:31 +0400

    Начал навг.меню.Доделать.

--- app/src/main/java/com/example/mydzdoublelessonsix/MainActivity.java
+++ app/src/main/java/com/example/mydzdoublelessonsix/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.mydzdoublelessonsix;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 
 import android.annotation.SuppressLint;
@@ -11,25 +14,87 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.google.android.material.navigation.NavigationView;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.contact_main);
+
+
     }
 
-    private void initToolbar() {
+    private void initView() {
+        Toolbar toolbar = initToolbar();
+        initDrawer(toolbar);
+
+    }
+    // регистрация drawer
+    private void initDrawer(Toolbar toolbar) {
+        final DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        // Обработка навигационного меню
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected( MenuItem item) {
+                int id = item.getItemId();
+                if (navigateFragment(id)){
+                    drawer.closeDrawer(GravityCompat.START);
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+
+
+
+
+    private Toolbar initToolbar() {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        return toolbar;
     }
 
     @SuppressLint("NonConstantResourceId")
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
+        if (navigateFragment(id)) {
+            return true;
+        }
+
         return super.onOptionsItemSelected(item);
     }
+
+
+
+    private boolean navigateFragment(int id) {
+        switch (id) {
+            case R.id.action_settings:
+                addFragment(new MonthsFragment());
+                return true;
+            case R.id.action_main:
+                addFragment(new FloraFragment());
+                return true;
+            case R.id.action_favorite:
+                addFragment(new FloraOrientation());
+                return true;
+
+        }
+        return false;
+    }
+
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main, menu);
